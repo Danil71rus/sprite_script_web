4905718cfd1697457c15260a22fefc9eb952e7dd
Данил Степин <dstepin@itoolabs.com>
2021-02-05 11:03:08 +0300

    dist

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const ip = require("ip");
 const fs = require('fs');
 const cors = require("cors");
 const express = require("express");
+const path = require('path');
 
 const config = require("./my.config");
 
@@ -10,16 +11,20 @@ const app = express();
 const hostname = config.serverFile.host;
 const port = config.serverFile.port;
 
+const htmlPath = path.join(__dirname, 'dist');
+app.use(express.static(htmlPath));
+
+
 global.__basedir = __dirname;
 const dir = __basedir + "/uploads";
 if (!fs.existsSync(dir)){
     fs.mkdirSync(dir);
 }
 
+
 const corsOptions = {
     origin: `http://${hostname}:${config.client.port}`,
 };
-
 app.use(cors(corsOptions));
 
 const initRoutes = require("./server/routes");
