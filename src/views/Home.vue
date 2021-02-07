50470b6819fa4882719aa3c550be3c10ff7a1351
Данил Степин <dstepin@itoolabs.com>
2021-02-07 21:56:43 +0300

    Добавлена инструкция

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="home">
+        <h3 @click="hideGif" title="Скрыть gif">Инструкция gif. Файлы только с расширением png!!!</h3>
+        <img class="setup-gif" v-show="isShowGif" :src="require(`@a/setup.gif`)" @click="hideGif"/>
         <upload-files></upload-files>
     </div>
 </template>
@@ -13,7 +15,10 @@ import UploadFiles from "@/components/controls/UploadFiles.vue"
     components: {UploadFiles, GButton}
 })
 export default class Home extends Vue {
-
+    isShowGif = true
+    hideGif() {
+        this.isShowGif = !this.isShowGif
+    }
 }
 </script>
 <style lang="scss">
@@ -23,6 +28,21 @@ export default class Home extends Vue {
     align-items: center;
     width: 80%;
     margin: auto;
+    .setup-gif {
+        width: 30%;
+        cursor: pointer;
+        transition: width 1s ease;
+        &:hover {
+            width: 90%;
+        }
+    }
+    h3 {
+        cursor: pointer;
+        &:hover {
+            color: #0f5498;
+        }
+        margin-bottom: 15px;
+    }
 }
 @media (max-width: 600px) {
     .home {
